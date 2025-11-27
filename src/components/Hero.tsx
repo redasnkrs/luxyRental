ac00e4ef079c146b9eb40c5e771816f2fac971b4
reda. <redadahmani34@gmail.com>
2025-11-27 11:17:21 +0100

    feat: new navbar and did some change on the hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,15 +1,16 @@
 export default function Hero() {
   return (
     <section className="bg-hero">
-      <div className="absolute mt-[680px]">
-        <h1 className=" text-9xl">
+      <div className="absolute mt-[640px] pl-3">
+        <h1 className=" lg:text-9xl text-7xl">
           The next level <br />
-          car <span className="text-[#EB3813] text-9xl">renting</span>
+          car{" "}
+          <span className="text-[#EB3813] lg:text-9xl text-7xl">renting</span>
         </h1>
-        <p className="text-[18px] font-light text-[#A2A2A2]">
+        <p className="text-[18px] font-light text-[#A2A2A2] text-hero">
           Experience unmacted confort, style and service.
         </p>
-        <p className="text-[18px] font-light text-[#A2A2A2]">
+        <p className="text-[18px] font-light text-[#A2A2A2] text-hero">
           Wherever the road take you.
         </p>
       </div>
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,23 +1,27 @@
+import Logo from "../assets/logo/luxyrental-logo-white.png";
+
 export default function NavBar() {
   return (
     <nav className="bg-transparent fixed top-0 w-full">
-      <div className="flex content-between items-center gap-4 justify-between">
-        <img src="/logo.png" alt="Luxy Rental Logo" />
-        <ul className="flex items-center gap-5 bg-red">
-          <li>
-            <a href="">Cars</a>
-          </li>
-          <li>
-            <a href="">News</a>
-          </li>
-          <li>
-            <a href="">About</a>
-          </li>
-          <li>
-            <a href="">Contact</a>
-          </li>
-        </ul>
-        <button>Book Now</button>
+      <div className="flex content-between items-center gap-4 justify-evenly">
+        <img src={Logo} className="mt-2" alt="Luxy Rental Logo" />
+        <div className="mt-2">
+          <ul className="flex items-center gap-5 text-white">
+            <li>
+              <a href="">Cars</a>
+            </li>
+            <li>
+              <a href="">News</a>
+            </li>
+            <li>
+              <a href="">About</a>
+            </li>
+            <li>
+              <a href="">Contact</a>
+            </li>
+          </ul>
+        </div>
+        <button className="mt-2.5">Book Now</button>
       </div>
     </nav>
   );
