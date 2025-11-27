0d8ab046d6e369be396df16f68efd8c9033331c6
reda. <redadahmani34@gmail.com>
2025-11-27 08:29:10 +0100

    fix: Fixed the image background for hero

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,6 +1,6 @@
 export default function NavBar() {
   return (
-    <nav className="bg-red-300 fixed top-0 w-full">
+    <nav className="bg-transparent fixed top-0 w-full">
       <div className="flex content-between items-center gap-4 justify-between">
         <img src="/logo.png" alt="Luxy Rental Logo" />
         <ul className="flex items-center gap-5 bg-red">
