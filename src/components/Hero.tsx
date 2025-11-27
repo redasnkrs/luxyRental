749d659d35b5329c0fe407e3fb40e806be4c7714
reda. <redadahmani34@gmail.com>
2025-11-27 10:16:09 +0100

    style: Added boska font

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,9 +1,17 @@
 export default function Hero() {
   return (
     <section className="bg-hero">
-      <div>
-        <h1>Welcome to Luxy Rental</h1>
-        <p>Find your dream car today!</p>
+      <div className="absolute mt-[680px]">
+        <h1 className=" text-9xl">
+          The next level <br />
+          car <span className="text-[#EB3813] text-9xl">renting</span>
+        </h1>
+        <p className="text-[18px] font-light text-[#A2A2A2]">
+          Experience unmacted confort, style and service.
+        </p>
+        <p className="text-[18px] font-light text-[#A2A2A2]">
+          Wherever the road take you.
+        </p>
       </div>
     </section>
   );
