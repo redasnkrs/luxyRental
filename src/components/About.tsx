bf5230ecba59a81679490f2c031910562956275a
reda. <redadahmani34@gmail.com>
2025-11-29 12:14:15 +0100

    style: Added about section and styked it (look good tbh)

---
+++ src/components/About.tsx
@@ -0,0 +1,24 @@
+export default function About() {
+  return (
+    <section className="mt-56 mb-56 lg:ml-[335px] sm:ml-5 relative">
+      <div className="mb-8">
+        <h2 className="text-4xl">
+          Dedicated <br />
+          To Perfection
+        </h2>
+        <p className="text-about">Experience with the top-notch service</p>
+        <p className="text-about">and unmatched car.</p>
+      </div>
+      <div className="flex justify-between flex-col lg:flex-row">
+        <p className="w-[507px] text-3xl text-about-2">
+          Lorem ipsum dolor sit amet consectetur. Sit donec tellus neque integer
+          congue aliquet eu. Enim hendrerit sed elit sed mauris eget sagittis.
+          Amet nunc arcu nisl eros hac tincidunt eu risus in. Sit suspendisse eu
+          duis ipsum tincidunt facilisis diam ac porta. Diam consectetur
+          imperdiet proin amet pellentesque vitae cras in.
+        </p>
+        <div className="about-pic mr-80 "></div>
+      </div>
+    </section>
+  );
+}
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,13 @@
 import Hero from "../components/Hero";
 import NavBar from "../components/NavBar";
+import About from "../components/About";
 
 function HomePage() {
   return (
     <div>
       <NavBar />
       <Hero />
+      <About />
     </div>
   );
 }
